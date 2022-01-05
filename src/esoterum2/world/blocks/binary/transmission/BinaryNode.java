da41664d549caa5946fa528e6572ad770d778fbc
Lele567 <48808663+Lele567@users.noreply.github.com>
2022-01-05 14:34:51 -0500

    yeet

--- src/esoterum2/world/blocks/binary/transmission/BinaryNode.java
+++ src/esoterum2/world/blocks/binary/transmission/BinaryNode.java
@@ -5,6 +5,7 @@ import arc.graphics.*;
 import arc.graphics.g2d.*;
 import arc.math.*;
 import arc.math.geom.*;
+import arc.struct.*;
 import arc.util.*;
 import arc.util.io.*;
 import esoterum2.*;
@@ -151,11 +152,11 @@ public class BinaryNode extends BinaryBlock{
         @Override
         public void placed(){
             super.placed();
-
             BinaryNodeBuild c = linkedNode();
             if(c != null && c.linkedNode() != this){
                 c.configure(this);
             }
+            updateProximity();
         }
 
         @Override
